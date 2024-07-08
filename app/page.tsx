2abf84be2796b0e20f311e2e5236319c2f5dc9f0
ShuriIkeno <nogi.akari0305@gmail.com>
2024-07-09 02:10:37 +0900

    "Initial commit"

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Page</h1>
+            <p>Page content</p>
+        </div>
+    )
+}
---
+++ app/tailwind-test/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './style.css'
+
+export default function Page() {
+  return <h1 className="text-6xl font-bold text-blue-600">Tailwind Test</h1>
+}
